4d6dde7ce5bfba73d7e79e681bea490ad60dbb22
Agata Ostolska <61022689+agaosto@users.noreply.github.com>
2021-04-30 13:42:14 +0200

    Update ImageFilter.java

--- fork_join_filter/src/fork_join_filter/ImageFilter.java
+++ fork_join_filter/src/fork_join_filter/ImageFilter.java
@@ -1,26 +1,27 @@
 package fork_join_filter;
 
 import java.util.concurrent.*;
+
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.highgui.HighGui;
 
 public class ImageFilter {
-	public static void main(String[] args) {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		
-		Mat original = Imgcodecs.imread("sunflowers.jpg",Imgcodecs.IMREAD_UNCHANGED);
-		Mat filtered = original.clone();
-		
-		HighGui.imshow("Original image",original);		
-		
-		final ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		RecursiveActionFilter recFilter = new RecursiveActionFilter(original,filtered);
-		forkJoinPool.submit(recFilter);
-		recFilter.join();
-		        
-        	HighGui.imshow("Filtered image",filtered);
-        	HighGui.waitKey();
-	}
+        Mat original = Imgcodecs.imread("sunflowers.jpg", Imgcodecs.IMREAD_UNCHANGED);
+        Mat filtered = original.clone();
+
+        HighGui.imshow("Original image", original);
+
+        final ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
+
+        RecursiveActionFilter recFilter = new RecursiveActionFilter(original, filtered);
+        forkJoinPool.submit(recFilter);
+        recFilter.join();
+
+        HighGui.imshow("Filtered image", filtered);
+        HighGui.waitKey();
+    }
 }
